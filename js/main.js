93b74528af8975141220dabc8c3b05b4d00c2934
William White, Jr <wewjr82@gmail.com>
2023-09-06 21:18:14 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function updatemenu() {
+  if (document.getElementById("responsive-menu").checked == true) {
+    document.getElementById("menu").style.borderBottomRightRadius = "0";
+    document.getElementById("menu").style.borderBottomLeftRadius = "0";
+  } else {
+    document.getElementById("menu").style.borderRadius = "0px";
+  }
+}
+
+document.querySelector("#contact-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  e.target.elements.name.value = "";
+  e.target.elements.email.value = "";
+  e.target.elements.message.value = "";
+});
+
