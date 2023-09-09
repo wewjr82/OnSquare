597c77019232c73903fcf35267eb86e0773011f2
William White, Jr <wewjr82@gmail.com>
2023-09-08 22:07:12 -0400

    adding mobile responsiveness to it

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
-function updatemenu() {
-  if (document.getElementById("responsive-menu").checked == true) {
-    document.getElementById("menu").style.borderBottomRightRadius = "0";
-    document.getElementById("menu").style.borderBottomLeftRadius = "0";
-  } else {
-    document.getElementById("menu").style.borderRadius = "0px";
-  }
-}
+// function updatemenu() {
+//   if (document.getElementById("responsive-menu").checked == true) {
+//     document.getElementById("menu").style.borderBottomRightRadius = "0";
+//     document.getElementById("menu").style.borderBottomLeftRadius = "0";
+//   } else {
+//     document.getElementById("menu").style.borderRadius = "0px";
+//   }
+// }
 
-document.querySelector("#contact-form").addEventListener("submit", (e) => {
-  e.preventDefault();
-  e.target.elements.name.value = "";
-  e.target.elements.email.value = "";
-  e.target.elements.message.value = "";
-});
+// document.querySelector("#contact-form").addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   e.target.elements.name.value = "";
+//   e.target.elements.email.value = "";
+//   e.target.elements.message.value = "";
+// });
+
 
